7fd93580a1118c2013bebb17801c7b36749fd19a
rutujadiwate <rutuja.22110140@viit.ac.in>
2024-09-02 20:11:38 +0530

    tour

---
+++ E_Plant_Shopping/src/plant.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './plant.css';
+
+function plant(){
+    return(
+        <div className='plant'>
+            <p className='des'>Welcome to Paradise Nursery, where green meets serenity!</p>
+            <p className='content'>
+            We are devoted to bringing nature closer to you here at Paradise Nursery. Our goal is to give a large selection of premium plants that improve the aesthetics of your environment while also promoting a more sustainable and healthful way of life. There is something for every plant enthusiast among us, including scented and air-purifying plants.
+            </p>
+            <p className='content'>Our team of professionals is committed to making sure that every plant satisfies our high requirements for care and quality. We're here to help you every step of the way, whether you're a novice or an experienced gardener. Please take a look around, ask any questions, and let us assist you in selecting the ideal plant for your house or place of business.
+            </p>
+            <p className='content'>Join us in transforming the world into one that is healthier and greener. Experience the splendor of nature at your doorstep by going to Paradise Nursery today.
+            </p>
+        </div>
+    );
+}
+export default plant;
